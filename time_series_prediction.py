5a6a22447321b53d22a278613c5774b38a7fe9ee
matstveter <mats.tveter@gmail.com>
2023-01-19 09:59:23 +0100

    Huge cleanup

--- time_series_prediction.py
+++ time_series_prediction.py
@@ -1,6 +1,16 @@
 from myPack.run_experiment import run_experiments
 from myPack.logging_info.parsing import setup_run
 
+
+def main():
+    data_dict, model_dict, hyper_dict, general_dict = setup_run()
+
+    if general_dict['test_mode']:
+        run_tests()
+    else:
+        run_experiments()
+
+
 if __name__ == "__main__":
     data_dict, model_dict, hyper_dict, time_dict, general_dict = setup_run()
     run_experiments(data_dict=data_dict,
