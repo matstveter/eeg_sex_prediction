3cce7d9a987afad31b46bc9549c6234a70212c40
matstveter <mats.tveter@gmail.com>
2023-06-23 09:03:51 +0200

    Clean-up, and removed the test-mode, only expeirment mode is kept

--- time_series_prediction.py
+++ time_series_prediction.py
@@ -14,12 +14,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = "2"
 def main():
     data_dict, model_dict, hyper_dict, time_dict, general_dict = setup_run()
 
-    if general_dict['test_mode']:
-        testing_models(data_dict=data_dict, model_dict=model_dict, hyper_dict=hyper_dict,
-                       time_dict=time_dict, general_dict=general_dict)
-    else:
-        run_final_experiments(data_dict=data_dict, model_dict=model_dict, hyper_dict=hyper_dict,
-                              time_dict=time_dict, general_dict=general_dict)
+    run_final_experiments(data_dict=data_dict, model_dict=model_dict, hyper_dict=hyper_dict,
+                          time_dict=time_dict, general_dict=general_dict)
 
 
 if __name__ == "__main__":
